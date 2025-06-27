a1a19d3eecf4561494b270ddaf58c376f06722da
peterson <peterson.aquino@compila.com.br>
2025-06-27 10:48:45 -0400

    first

--- src/app/currency-converter/currency-converter.ts
+++ src/app/currency-converter/currency-converter.ts
@@ -16,16 +16,32 @@ export class CurrencyConverter {
   valor = 0;
   resultado = 0;
   direcao: 'brlToClp' | 'clpToBrl' = 'brlToClp';
-  cotacao = 175;
+  cotacao = 171;
+  valorbkp = 0;
 
   converter() {
-    this.resultado = this.direcao === 'brlToClp'
-      ? this.valor * this.cotacao
-      : this.valor / this.cotacao;
+    if (this.direcao === 'brlToClp') {
+      this.resultado = this.valor * this.cotacao;
+    } else {
+      this.resultado = this.valor / this.cotacao;
+    }
+
+
   }
 
   selectAll(input: HTMLInputElement) {
     input.select();
+  }
+
+  onDirecaoChange(novaDirecao: 'brlToClp' | 'clpToBrl') {
+    // Só aplica se já existe um resultado calculado
+    if (this.resultado > 0) {
+        this.valorbkp = this.valor
+        this.valor = this.resultado
+        this.resultado = this.valorbkp
+    }
+    this.direcao = novaDirecao;
+    
   }
 
 }
